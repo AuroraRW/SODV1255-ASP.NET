88d585bf003796c4f05cc37558d2bc6fe232d78f
AuroraRW <aurorawangca@gmail.com>
2024-02-02 17:04:04 -0700

    add ModelApp

---
+++ ModelAPP/ModelAPP/Controllers/BookController.cs
@@ -0,0 +1,127 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ModelAPP.Models;
+namespace ModelAPP.Controllers
+{
+
+    public class BookController : Controller
+    {
+        ////Author is route parameter, bookid is query string
+        //[Route("/Books/{Author}")]
+        //public IActionResult Book()
+        //{
+        //    int? bookid = Convert.ToInt32(Request.Query["bookid"]);
+        //    string? author = Convert.ToString(Request.RouteValues["Author"]);
+        //    return Content($"Book ID is:{bookid}\nAuthor is: {author}", "text/plain");
+        //}
+
+        //// pass the query string to the parameter of action method, case insensitive
+        //[Route("/Books")]
+        //public IActionResult Book(int? bookid, string? author)
+        //{
+        //    return Content($"Book ID is:{bookid}\nAuthor is: {author}", "text/plain");
+        //}
+
+        //// pass the route parameter to the parameter of action method, case insensitive
+        //[Route("/Books/{BookId}/{Author}")]
+        //public IActionResult Book(int? bookid, string? author)
+        //{
+        //    return Content($"Book ID is:{bookid}\nAuthor is: {author}", "text/plain");
+        //}
+
+        // if both provided, route parameter has higher priority 
+
+        //// one from query string , one from route parameter
+        //[Route("/Books/{BookId}/{Author}")]
+        //public IActionResult Book([FromQuery] int? bookid, [FromRoute] string? author)
+        //{
+        //    return Content($"Book ID is:{bookid}\nAuthor is: {author}", "text/plain");
+        //}
+
+        ////if route parameter not provided, bookid is null
+        //[Route("/Books/{BookId?}/{Author?}")]
+        //public IActionResult Book([FromRoute] int? bookid, [FromQuery] string? author)
+        //{
+        //    if (bookid.HasValue == false)
+        //    {
+        //        return Content("ID is not found", "text/plain");
+        //    }
+        //    return Content($"Book ID is:{bookid}\nAuthor is: {author}", "text/plain");
+        //}
+
+        //// one from route parameter, one from query string, update the code in model
+        //[Route("/Books/{BookId?}/{Author?}")]
+        //public IActionResult Book(Book book)
+        //{
+        //    //if (book.BookId.HasValue == false)
+        //    //{
+        //    //    return Content("ID is not found", "text/plain");
+        //    //}
+        //    return Content($"Book ID is:{book.BookId}\nAuthor is: {book.Author}", "text/plain");
+        //}
+
+        ///////////////////////////////Form data binding////////////////////
+        //[Route("/Books/{BookId?}/{Author?}")]
+        //public IActionResult Book(Book book)
+        //{
+        //    if (book.BookId.HasValue == false)
+        //    {
+        //        return Content("ID is not found", "text/plain");
+        //    }
+        //    return Content($"Book ID is:{book.BookId}\nAuthor is: {book.Author}", "text/plain");
+        //}
+
+        ///////////////////////////////Model validation////////////////////
+        //[Route("/Books/{BookId?}/{Author?}")]
+        //public IActionResult Book(Book book)
+        //{
+        //    if (book.BookId.HasValue == false)
+        //    {
+        //        return Content("ID is not found", "text/plain");
+        //    }
+
+        //    //// Validate manually
+        //    //if (string.IsNullOrEmpty(book.BookName))
+        //    //{
+        //    //    return BadRequest("BookName is required!");
+        //    //}
+
+        //    if (!ModelState.IsValid)
+        //    {
+        //        return BadRequest("BookName is required!");
+        //    }
+        //    return Content($"Book ID is:{book.BookId}\nAuthor is: {book.Author}", "text/plain");
+        //}
+
+
+        ////////////////////////////Catch error//////////////////////////
+        [Route("/Books/{BookId?}/{Author?}")]
+        public IActionResult Book(Book book)
+        {
+            if (book.BookId.HasValue == false)
+            {
+                return Content("ID is not found", "text/plain");
+            }
+
+            //// Validate manually
+            //if (string.IsNullOrEmpty(book.BookName))
+            //{
+            //    return BadRequest("BookName is required!");
+            //}
+
+            if (!ModelState.IsValid)
+            {
+                List<string> errors = new List<string>();
+                foreach (var value in ModelState.Values)
+                {
+                    foreach (var error in value.Errors)
+                    {
+                        errors.Add(error.ErrorMessage);
+                    }
+                }
+                string errorMessages = string.Join("\n", errors);
+                return BadRequest(errorMessages);
+            }
+            return Content($"Book ID is:{book.BookId}\nAuthor is: {book.Author}\nBookName is: {book.BookName}\nPrice is: {book.Price}\n", "text/plain");
+        }
+    }
+}
---
+++ ModelAPP/ModelAPP/Models/Book.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace ModelAPP.Models
+{
+    //////////////Model binding//////////////////
+    //public class Book
+    //{
+    //    [FromRoute]
+    //    public int? BookId { get; set; }
+
+    //    [FromQuery]
+    //    public string? Author { get; set; }
+    //}
+
+    ////////////////Form data binding////////////////////
+    //public class Book
+    //{
+    //    public int? BookId { get; set; }
+
+    //    public string? Author { get; set; }
+    //}
+
+    //////////////////Model Validation///////////////
+    //public class Book
+    //{
+    //    public int? BookId { get; set; }
+    //    [Required]
+    //    public string BookName { get; set; }
+    //    public string? Author { get; set; }
+    //}
+
+    ///////////////////show error message/////////////
+    //public class Book
+    //{
+    //    public int? BookId { get; set; }
+    //    [Required(ErrorMessage = "{0} should be not empty")]
+    //    [Display(Name = "The name of book")]
+    //    public string BookName { get; set; }
+    //    public string? Author { get; set; }
+    //}
+
+    /////////////////build in model validation////////////
+    public class Book
+    {
+        public int? BookId { get; set; }
+        [Required(ErrorMessage = "{0} is required")]
+        [Display(Name = "Book Name")]
+        [StringLength(40, MinimumLength = 3, ErrorMessage = "{0} should be between {2} and {1} characters long")]
+        public string BookName { get; set; }
+        public string? Author { get; set; }
+
+        [Range(0, 999.99, ErrorMessage = "{0} should be between ${1} and ${2}")]
+        public double? Price { get; set; }
+
+        [EmailAddress(ErrorMessage = "{0} should be a proper email address")]
+        [Required(ErrorMessage = "{0} can't be blank")]
+        public string AuthorEmail { get; set; }
+
+
+        [Required(ErrorMessage = "{0} can't be blank")]
+        public string? Password { get; set; }
+
+
+        [Required(ErrorMessage = "{0} can't be blank")]
+        [Compare("Password", ErrorMessage = "{0} and {1} do not match")]
+        [Display(Name = "Re-enter Password")]
+        public string? ConfirmPassword { get; set; }
+    }
+
+}
